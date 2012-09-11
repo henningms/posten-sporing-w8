982d7f072652bfef2a4afa5d94977449cd4ba14a
Henning M. Stephansen <henningms@gmail.com>
2012-09-11 22:16:52 +0200

    initial commit

---
+++ Sporingsappen/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using PostenApi.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Sporingsappen
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<TrackingItem> items = new ObservableCollection<TrackingItem>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Page_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            gridView.ItemsSource = items;
+        }
+
+        private async void Track(String id)
+        {
+            var package = await PostenApi.Service.Tracking.Track(id);
+
+            items.Add(package);
+            
+
+        }
+
+        private void btnSearch_Click_1(object sender, RoutedEventArgs e)
+        {
+            Track(txtAddTrackingNumber.Text.Trim());
+        }
+    }
+}
